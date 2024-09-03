d9c35e34fc5978b32f06e12742b70b2b45452e3f
aditya <adityav@cdac.in>
2024-09-03 14:29:02 +0530

    My First commit

---
+++ Demo-Boot-Project/src/main/java/com/app/DemoBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootProjectApplication.class, args);
+	}
+
+}
